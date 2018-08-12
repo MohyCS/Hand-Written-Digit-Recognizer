01c61e98de650f138008baeef46371edd8f0905e
Mohaymen Ahmed <34009686+MohyCS@users.noreply.github.com>
2018-08-12 15:13:30 -0400

    Update tf_train_model_ASSIGNMENT_FILE_EXTRACREDIT.py

--- tf_train_model_ASSIGNMENT_FILE_EXTRACREDIT.py
+++ tf_train_model_ASSIGNMENT_FILE_EXTRACREDIT.py
@@ -118,18 +118,22 @@ class build_train:
         for i in range(1000): #should be 20,000
             batch = mnist.train.next_batch(50)
             if i%100 == 0:
+
+                batch = mnist.train.next_batch(50)
                 train_accuracy = accuracy.eval(feed_dict={x:batch[0], y_: batch[1], keep_prob: 1.0})
                 train_eval.append(train_accuracy)
 
+                batch = mnist.train.next_batch(50)
                 test_accuracy = accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0})
                 test_eval.append(test_accuracy)
 
                 time.append(i)
 
             print("step %d, training accuracy %g"%(i, train_accuracy))
+            print("step %d, testing accuracy %g"%(i, test_accuracy))
             train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
-        print("test accuracy %g"%accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+
         ############# END OF TRAINING SESSION ##############################
 
         ############# SAVE MODEL ###########################################
